d987ea08327d7e1c33bc894e4935fd3e1a650e4a
Tristan Goodell <>
2024-05-20 14:48:28 -0400

    Started Nuxt project. Started planning. Added Mocks Server, Primevue, etc.

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import 'primeicons/primeicons.css'
+</script>
+
+<template>
+  <div>
+    <Button label="Check" icon="pi pi-check" />
+  </div>
+  <Glossary />
+</template>
---
+++ components/Glossary.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// Glossary with Definitions and Terms
+    // Features:
+        // Add term + definition
+        // Edit term + definition
+        // Bulk import
+        // Search
+    // Strategy:
+        // Create API endpoint with Mocks Server
+
+</script>
+
+<template>
+    Glossary!
+</template>
