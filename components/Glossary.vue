eadfa388cc3a6f9bd62955d1a67086522199382a
Tristan Goodell <>
2024-05-21 09:37:26 -0400

    Fetching and printing mocked data

--- components/Glossary.vue
+++ components/Glossary.vue
@@ -7,9 +7,22 @@
         // Search
     // Strategy:
         // Create API endpoint with Mocks Server
+        // $fetch it
 
+const glossary = ref()        
+async function getGlossary() {
+  const data = await $fetch('http://localhost:3100/api/glossary', {
+    method: 'GET',
+  })
+  glossary.value = data
+}
+
+getGlossary()
 </script>
 
 <template>
     Glossary!
+    <div v-for="entry in glossary" >
+        {{ entry.term }} - {{ entry.definition }}
+    </div>
 </template>
